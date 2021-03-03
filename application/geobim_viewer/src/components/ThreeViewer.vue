03498def7d280450cf92b1398880227a889e1366
jliempt <jordi_purdi@live.nl>
2021-03-03 18:25:27 +0100

    Load IFC GLB from server

--- application/geobim_viewer/src/components/ThreeViewer.vue
+++ application/geobim_viewer/src/components/ThreeViewer.vue
@@ -2,7 +2,8 @@
   <div id="canvas" />
 </template>
 
-<script>
+<script lang="ts">
+
 import {
 	Scene,
 	Color,
@@ -28,11 +29,181 @@ import {
 	SpriteMaterial
 } from 'three';
 
+import Vue from 'vue'
+import Component from 'vue-class-component'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
-export default {
-	name: 'ThreeViewer',
+@Component({
+
+})
+
+export default class ThreeViewer extends Vue {
+
+	scene: Scene;
+	renderer: WebGLRenderer;
+
+	initScene() {
+
+		this.scene = new Scene();
+		this.scene.background = new Color( 0xffffff );
+
+		const canvas = document.getElementById( "canvas" );
+
+		this.renderer = new WebGLRenderer( { antialias: false } );
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( canvas!.clientWidth, canvas!.clientHeight );
+		this.renderer.setClearColor( 0xd9eefc );
+		this.renderer.outputEncoding = sRGBEncoding;
+		this.renderer.autoClear = false;
+
+		canvas!.appendChild( this.renderer.domElement );
+
+		// this.renderer.render( this.scene, this.camera );
+		
+	}
+
+	mounted() {
+
+		this.initScene();
+
+	}
+
+	loadglTF( url: string ) {
+
+		const loader = new GLTFLoader();
+		
+		// var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+		const isIE11 = false;
+		
+		if (!isIE11) {
+
+			const draco = new DRACOLoader;
+
+			draco.setDecoderPath( 'https://unpkg.com/three@0.126.0/examples/js/libs/draco/gltf/' );
+			loader.setDRACOLoader(draco);
+
+		}            
+		
+		loader.load( url, gltf => {
+
+				console.log( "hoi" );
+
+				this.scene.add(gltf.scene);
+
+				console.log( gltf );
+
+				// var createdLines = {};
+				// var geometryCount = {};
+
+				// gltf.scene.traverse((obj) => {
+				// 	if (obj.isMesh && obj.geometry) {
+				// 		geometryCount[obj.geometry.id] = 1;
+				// 	}
+				// });
+
+				// // @todo we'll make this more adaptive and pregenerate the lines in gltf.
+				// var createLines = Object.keys(geometryCount).length <= 500;
+				// if (!createLines) {
+				// 	console.log("not creating line geometries due to model size");
+				// }
+
+				// gltf.scene.traverse((obj) => {
+				// 	if (obj.isMesh && obj.geometry) {
+				// 		self.originalMaterials.set(obj.id, obj.material);
+				// 		obj.material.side = THREE.DoubleSide;
+				// 		obj.material.depthWrite = !obj.material.transparent;
+
+				// 		if (createLines) {
+				// 			var edges;
+				// 			if (obj.geometry.id in createdLines) {
+				// 				edges = createdLines[obj.geometry.id];
+				// 			} else {
+				// 				edges = createdLines[obj.geometry.id] = new THREE.EdgesGeometry(obj.geometry);
+				// 			}
+				// 			var line = new THREE.LineSegments(edges, lineMaterial);
+				// 			obj.add(line);
+				// 		}                            
+				// 	}
+
+				// 	if (obj.name.startsWith("product-")) {
+				// 		const id2 = obj.name.substr(8, 36);
+				// 		const g = Utils.CompressGuid(id2);
+				// 		self.allIds.push(g);
+				// 		self.nameToId.set(g, obj.id);
+				// 		self.nameToId.set(obj.name, obj.id);
+				// 	}
+				// });
+
+				// if (first) {
+
+				// 	var boundingBox = new THREE.Box3();
+				// 	boundingBox.setFromObject(scene);
+				// 	var center = new THREE.Vector3();
+				// 	boundingBox.getCenter(center);
+				// 	controls.target = center;
+					
+				// 	// An initial for viewer distance based on the diagonal so that
+				// 	// we have a camera matrix for a more detailed calculation.
+				// 	var viewDistance = boundingBox.getSize(new THREE.Vector3()).length();
+				// 	camera.position.copy(center.clone().add(
+				// 		new THREE.Vector3(0.5, 0.25, 1).normalize().multiplyScalar(viewDistance)
+				// 	));
+					
+				// 	// Make sure all matrices get calculated.
+				// 	camera.near = viewDistance / 100;
+				// 	camera.far = viewDistance * 100;
+				// 	controls.update();
+				// 	camera.updateProjectionMatrix();
+				// 	camera.updateMatrixWorld();
+					
+				// 	var fovFactor = Math.tan(camera.fov / 2 / 180 * 3.141592653);
+				// 	var outside = 0.;
+					
+				// 	// Calculate distance between projected bounding box coordinates and view frustrum boundaries
+				// 	var largestAngle = 0.;
+				// 	for (var i = 0; i < 8; i++) {
+				// 		const v = new THREE.Vector3(
+				// 			i & 1 ? boundingBox.min.x : boundingBox.max.x,
+				// 			i & 2 ? boundingBox.min.y : boundingBox.max.y,
+				// 			i & 4 ? boundingBox.min.z : boundingBox.max.z
+				// 		);
+				// 		v.applyMatrix4(camera.matrixWorldInverse);
+				// 		// largestAngle = Math.max(largestAngle, Math.atan2(v.x / camera.aspect, -v.z), Math.atan2(v.y, -v.z));
+				// 		outside = Math.max(outside, Math.abs(v.x / camera.aspect) - fovFactor * -v.z, Math.abs(v.y) - fovFactor * -v.z);
+				// 		console.log(v.x / camera.aspect, fovFactor * -v.z);
+				// 	}
+					
+				// 	viewDistance += outside * 2;
+					
+				// 	camera.position.copy(center.clone().add(
+				// 		new THREE.Vector3(0.5, 0.25, 1).normalize().multiplyScalar(viewDistance)
+				// 	));
+
+				// 	controls.update();
+					
+				// 	first = false;
+				// }
+				
+				// self.fire("loaded");
+			},
+
+			// called while loading is progressing
+			function(xhr) {
+				console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+			},
+
+			// called when loading has errors
+			function(error) {
+				console.log('An error happened', error);
+			}
+		);
+
+	}
+
 }
+
 </script>
 
 <style scoped>
--- application/geobim_viewer/src/views/Viewer.vue
+++ application/geobim_viewer/src/views/Viewer.vue
@@ -6,7 +6,7 @@
 
         <div class="file">
           <label class="file-label">
-            <input class="file-input" type="file" name="resume" ref="ifcFile" @change="selectedFile">
+            <input class="file-input" type="file" name="resume" ref="ifcFiles" @change="selectedFile">
             <span class="file-cta">
               <span class="file-label">
                 Upload IFC
@@ -16,6 +16,11 @@
         </div>
 
       </div>
+
+      <!-- <div class="column is-main-content">
+        <ThreeViewer
+          ref="threeviewer"
+        /> -->
 
       <div class="column is-main-content">
         <ThreeViewer
@@ -28,22 +33,78 @@
 </template>
 
 <script lang="ts">
+
 import { Component, Vue } from 'vue-property-decorator';
 import ThreeViewer from '@/components/ThreeViewer.vue';
 import axios from 'axios';
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    ThreeViewer,
+    ThreeViewer
   },
 })
+
 export default class Viewer extends Vue {
+
+
+
+  baseURL = "http://127.0.0.1:5000/";
+
+  poll( url: string ) {
+
+    fetch( url )
+
+    .then(function(r) { return r.json(); })
+
+    .then(function(r: any) {
+
+      if ( r.progress === 100 ) {
+
+        const split = url.split( "/" );
+        const id = split[ split.length - 1 ];
+        
+        this.getModelInfo( id );
+
+        return;
+
+      }
+
+      setTimeout( () => {
+
+        console.log( r.progress );
+
+        this.poll( url );
+
+      }, 1000);
+
+    }.bind( this ));
+
+  }
+
+  getModelInfo( id: string ) {
+
+    const url = this.baseURL + "/v/" + id;
+    console.log( url );
+
+    fetch( this.baseURL + "/log/" + id + ".json" )
+    .then(function(r) { return r.text(); })
+    .then(function(log) {
+
+      // console.log( log );
+
+    });
+
+    const glbUrl = this.baseURL + "/m/" + id + "_0.glb"
+
+    this.$refs.threeviewer.loadglTF( glbUrl );
+
+
+  }
 
   async selectedFile() {
 
-    console.log("jep");
-    const file = this.$refs.ifcFile.files[0];
+    const files: any = this.$refs.ifcFiles; 
+    const file = files.files[0];
 
     if ( !file || file.name.slice(-4) != ".ifc" ) {
 
@@ -55,17 +116,33 @@ export default class Viewer extends Vue {
     const form = new FormData();
     form.append("file", file);
 
-    axios.post('http://127.0.0.1:5000/upload_ifc', form, {
+    axios.post( this.baseURL + 'upload_ifc', form, {
+
         headers: {
+
           'Content-Type': 'multipart/form-data'
+
         }
+
     })
-    .then(console.log)
+
+    .then( function ( res: any ) {
+
+      const url = res.data[ 'url' ];
+
+      this.poll( this.baseURL + url );
+
+    }
+    
+    .bind( this ))
     .catch(console.error);
 
   }
 
+
+
 }
+
 </script>
 
 <style scoped>
