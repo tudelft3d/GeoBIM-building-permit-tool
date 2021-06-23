c9adc36206adc859748271828f5e8b64b4306bc1
jliempt <jordi_purdi@live.nl>
2021-06-23 14:50:06 +0200

    Re-add Buefy

--- application/geobim_viewer/src/main.js
+++ application/geobim_viewer/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
 import './styles/bulma.scss';
 import App from './App.vue';
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Buefy);
 
 new Vue({
   router,
